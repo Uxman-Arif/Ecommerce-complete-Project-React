4f2db5a113f48b75d69a30aa3fadc245290c30f0
Usman Arif <usmn2391@gmail.com>
2025-03-24 12:38:17 +0500

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,12 @@ import { PrimeReactProvider } from 'primereact/api'
 import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
-import TemplateDemo from './components/menubar.jsx';
 
 createRoot(document.getElementById('root')).render(
   <PrimeReactProvider>
     <StrictMode>
-      <TemplateDemo />
       <App />
+      <h1 className='text-red-600 font-bold text-center animate-bounce'>Testing tailwind</h1>
     </StrictMode>
   </PrimeReactProvider>
 )
